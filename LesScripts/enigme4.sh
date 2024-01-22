05a20fc88a6c6b972416bfc34816c34e5ea0a9b7
Lhuissier Rémi <remi.lhuissier@etu.unilim.fr>
2024-01-22 10:40:50 +0100

    ajout 4eme enigme + début du readme

--- LesScripts/enigme4.sh
+++ LesScripts/enigme4.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
 
+printf "Je suis un langage qui communique avec les machines,\nLes développeurs m'utilisent pour créer des lignes.\nJe suis interprété ou compilé, c'est selon,\nLes boucles et les conditions, je les comprends bien.\n\nQui suis-je ?\n"
+
+reponse=""
+bonneReponse=false
+
+lesReponses=("Un code" "Un Code" "un code" "UN CODE" "un Code" "code" "Code" "CODE")
+
+
+while ([ "$bonneReponse" == false ] )
+do
+    echo 'Réponse : '
+    read reponse
+    for i in "${lesReponses[@]}"
+    do
+        #echo "[ "$i" == "$reponse" ]"
+        if ([ "$i" == "$reponse" ])
+            then 
+                bonneReponse=true
+        fi
+    done
+    if [ "$bonneReponse" == false ]
+        then
+            echo "Mauvaise réponse ! (ou peut être est-ce une faute de frappe ?)"
+    else
+        echo "Bonne reponse ! La lettre secrete est : O"
+    fi
+done
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # JPOProgrammesBash
+Le but de ce programme est de résoudres des petites énigmes afin de découvrir le mot "INFO".\
+Il faut, pour cela lancer le programme "menu.sh" sur linux (c'est un script bash).\
+Veillez à avoir les droits d'échecutions des programmes.\
+La commande pour modifier les droits est "chmod a+x nomDuFichier".
+
+Pour le lancer, mettez vous dans le répertoire et entrez dans le shell "./menu.sh".\
+Il suffit ensuite de choisir l'énigme que vous souhaitez lancer.
+
+## Les énigmes
