d9f2fbc00df0b54f828ec79f2c79170f4350141f
Lhuissier Rémi <remi.lhuissier@etu.unilim.fr>
2024-01-22 10:40:50 +0100

    ajout 4eme enigme +bugfixe guillemets enigme 3+ début du readme

--- LesScripts/enigme3.sh
+++ LesScripts/enigme3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf "def maFonction(chaine):\n    resultat = \n    for i in range (len(chaine)-1,-1,-1):\n        resultat+=chaine[i]\n    return resultat\n\nchaine = \"maChaine\"\nprint(maFonction(chaine))\n\n"
+printf "def maFonction(chaine):\n    resultat = \"\"\n    for i in range (len(chaine)-1,-1,-1):\n        resultat+=chaine[i]\n    return resultat\n\nchaine = \"maChaine\"\nprint(maFonction(chaine))\n\n"
 echo "Que fait ce code ? (Entrez le chiffre associé à la bonne réponse)"
 echo "1: Afficher une chaine de caractères aléatoires"
 echo "2: Inverser une chaine de caractères puis l'afficher"
